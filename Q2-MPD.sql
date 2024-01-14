3a23f70abac539bcd1f9309ddf0b147e84f4aca8
Chakib <ch.djeziri@gmail.com>
2024-01-14 22:08:51 +0100

    tp_mcd_mld_mpd_vf

--- Q2-MPD.sql
+++ Q2-MPD.sql
@@ -1,27 +1,24 @@
--- TP CHAKIB DJEZIRI
--- MPD Exercice 2
--- création de la table medecin
-CREATE TABLE medecin(
+CREATE TABLE Medecin(
     id_medecin INTEGER PRIMARY KEY AUTOINCREMENT,
     nom VARCHAR (255) NOT NULL 
 );
--- création de la table malade
-CREATE TABLE malade(
+
+CREATE TABLE Malade(
     id_malade INTEGER PRIMARY KEY AUTOINCREMENT,
     nom VARCHAR (255) NOT NULL 
 );
--- création de la table symptome
-CREATE TABLE symptome(
+
+CREATE TABLE Symptome(
     id_symptome INTEGER PRIMARY KEY AUTOINCREMENT,
     nom VARCHAR (255) NOT NULL 
 );
--- création de la table maladie
-CREATE TABLE maladie(
+
+CREATE TABLE Maladie(
     id_maladie INTEGER PRIMARY KEY AUTOINCREMENT,
     nom VARCHAR (255) NOT NULL 
 );
--- création de la table consultation
-CREATE TABLE consultation(
+
+CREATE TABLE Consultation(
     id_consultation INTEGER PRIMARY KEY AUTOINCREMENT,
     date DATE  NOT NULL DEFAULT CURRENT_DATE,
     prix DECIMAL NOT NULL,
@@ -30,41 +27,22 @@ CREATE TABLE consultation(
     FOREIGN KEY (id_medecin) REFERENCES medecin(id_medecin),
     FOREIGN KEY (id_malade) REFERENCES malade(id_malade)
 );
--- création de la table effectuer (relation many-to-many)
-CREATE TABLE effectuer(
-    id_effectuer INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_medecin INTEGER,
-    id_consultation INTEGER,
-    FOREIGN KEY (id_consultation) REFERENCES consultation(id_consultation),
-    FOREIGN KEY (id_medecin) REFERENCES medecin(id_medecin)
-);
--- création de la table concerner (relation many-to-many)
-CREATE TABLE concerner(
-    id_concerner INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_consultation INTEGER,
-    id_malade INTEGER,
-    FOREIGN KEY (id_consultation) REFERENCES consultation(id_consultation),
-    FOREIGN KEY (id_malade) REFERENCES malade(id_malade)
-);
--- création de la table detection (relation many-to-many)
-CREATE TABLE detecter(
-    id_detecter INTEGER PRIMARY KEY AUTOINCREMENT,
+
+CREATE TABLE Detecter(
     id_consultation INTEGER,
     id_symptome INTEGER,
     FOREIGN KEY (id_consultation) REFERENCES consultation(id_consultation),
     FOREIGN KEY (id_symptome) REFERENCES symptome(id_symptome)
 );
--- création de la table diagnostique (relation many-to-many)
-CREATE TABLE diagnostiquer(
-    id_diagnostiquer INTEGER PRIMARY KEY AUTOINCREMENT,
+
+CREATE TABLE Diagnostiquer(
     id_consultation INTEGER,
     id_maladie INTEGER,
     FOREIGN KEY (id_consultation) REFERENCES consultation(id_consultation),
     FOREIGN KEY (id_maladie) REFERENCES maladie(id_maladie)
 );
--- création de la table association (relation many-to-many)
-CREATE TABLE associer(
-    id_associer INTEGER PRIMARY KEY AUTOINCREMENT,
+
+CREATE TABLE Associer(
     id_symptome INTEGER,
     id_maladie INTEGER,
     FOREIGN KEY (id_symptome) REFERENCES symptome(id_symptome),
--- Q3-MPD.sql
+++ Q3-MPD.sql
@@ -1,40 +1,22 @@
--- MPD en SQLite
--- creation des tables puis ajout des relations
-
-CREATE TABLE proprietaire(
-    id_proprietaire INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE Proprietaire(
+    id_Proprietaire INTEGER PRIMARY KEY AUTOINCREMENT,
     nom VARCHAR (255) NOT NULL,
     prenom VARCHAR (255) NOT NULL,
     adresse TEXT (500) NOT NULL,
     tel TINYINT (25) NOT NULL
 );
 
-CREATE TABLE locataire(
-    id_locataire INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE Contrat(
+    id_Contrat INTEGER PRIMARY KEY AUTOINCREMENT,
+    duree INT (50000) NOT NULL,
+    poucentage TINYINT(100) NOT NULL,
+    adresse TEXT NOT NULL,
+    taille SMALLINT (6500) NOT NULL,
+    prix_de_location INT (1000000) NOT NULL,
+    periode_determinee INT (50000) NOT NULL,
     nom VARCHAR (255) NOT NULL,
     prenom VARCHAR (255) NOT NULL,
     adresse TEXT (500) NOT NULL,
     telephone TINYINT (25) NOT NULL
-);
-
-CREATE TABLE immeuble(
-    id_immeuble INTEGER PRIMARY KEY AUTOINCREMENT,
-    adresse TEXT NOT NULL,
-    taille SMALLINT (6500) NOT NULL,
-    prix_de_location INT (1000000) NOT NULL,
-    contrat_gestion TEXT(6500) NOT NULL,
-    contrat_gestion_duree INT (50000) NOT NULL,
-    contrat_gestion_poucentage TINYINT(100) NOT NULL,
-    id_proprietaire INTEGER,
-    FOREIGN KEY (id_proprietaire) REFERENCES proprietaire(id_proprietaire)
-);
-
-CREATE TABLE contrat_bail(
-    id_contrat_bail INTEGER PRIMARY KEY AUTOINCREMENT,
-    numero  TINYINT (255) NOT NULL,
-    periode INT (50000) NOT NULL,
-    id_proprietaire INTEGER,
-    id_locataire INTEGER,
-    FOREIGN KEY (id_proprietaire) REFERENCES proprietaire(id_proprietaire),
-    FOREIGN KEY (id_locataire) REFERENCES locataire(id_locataire)
+    FOREIGN KEY (id_Proprietaire) REFERENCES Proprietaire(id_Proprietaire)
 );
